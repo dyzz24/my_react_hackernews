11197c0c0d5d1a9372726deb7cbc219da6fa0537
dvasilkov <dvasilkov@kraftway.ru>
2019-12-06 14:07:14 +0300

    userinfo

--- src/userinfo/userinfo.js
+++ src/userinfo/userinfo.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import './userinfo.css'
+import './userinfo.css';
+import Httpservice from '../httpservice/httpservice';
+
 
 export class Userinfo extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {userData: {}}
 
   }
+
+  service = new Httpservice();
 
   componentDidMount() {
     // const test = this.match;
     const userId = this.props.match.params.id;
+    this.getUserInfo(userId)
     console.log(userId)
+  }
+
+  getUserInfo(userId) {
+    this.service
+          .getData(
+            `https://hacker-news.firebaseio.com/v0/user/${userId}.json?print=pretty`
+          )
+          .then(userInfo => {console.log(userInfo)
+          this.setState({userData: userInfo});
+          });
+  }
+  
+  timePipe = (time) => {
+    const data = new Date(time * 1000).toUTCString();
+    return data;
   }
 
 
@@ -21,7 +41,24 @@ export class Userinfo extends React.Component {
   return (
 
     <div className = 'userpopup'>
-    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div className = 'user'>
+      <div className = 'user__info'>
+        <div className = 'header'></div>
+      <div className = 'user__avatar'>
+        <div className = 'user__name'>{this.state.userData.id}</div>
+      </div>
+      <div className = 'info__txt'>
+        <p>О пользователе:</p>
+          <p className = 'maintxt'>{this.state.userData.about}</p>
+
+          <p>Зарегистрирован:</p>
+          <p className = 'maintxt'>{this.timePipe(this.state.userData.created)}</p>
+
+          <p>Репутация:</p>
+          <p className = 'maintxt'>{this.state.userData.karma}</p>
+      </div>
+      </div>
+    </div>
     </div>
 
 
